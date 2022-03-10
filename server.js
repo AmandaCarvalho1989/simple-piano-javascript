ddd15c9db958005f440a1d67b882d36f8ca2558b
Amanda Carvalho <amandamvcarvalho1989@gmail.com>
2022-03-09 22:25:58 -0300

    add recording feature

--- server.js
+++ server.js
@@ -1,11 +1,17 @@
 const express = require('express')
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://localhost/songRecorder')
 const app= express()
 
 app.set('view engine', 'ejs')
+app.use(express.json())
 app.use(express.static('public'))
 
 app.get('/', (req,res) =>{
   res.render('index')
 })
-
+app.post('/songs', (req,res)=>{
+  req.body.songNotes
+})
 app.listen(5000)
